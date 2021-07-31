45c507ea3f228cae91500f015db795412dc77b62
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-31 16:54:16 -0300

    formatacação dos botões da página inicial

--- verben/src/components/config.vue
+++ verben/src/components/config.vue
@@ -1,95 +1,128 @@
 <template>
-    <v-card>
-        {{temposVerbais}}
-<v-row>
-    <v-col>
-        <h2 class = "tituloTempos"> Indicativ </h2>
-    <v-checkbox
-    v-model="temposVerbais"
-      label="Prasens"
-      value = "PRASENS">
-      </v-checkbox>
-<v-checkbox
-    v-model="temposVerbais"
-      label="Prateritum"
-      value = "PRATERITUM">
-      </v-checkbox>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Futur1"
-      value = "FUTUR1">
-      </v-checkbox>
-        <v-checkbox
-    v-model="temposVerbais"
-      label="Futur2"
-      value = "FUTUR2">
-      </v-checkbox>
- <v-checkbox
-    v-model="temposVerbais"
-      label="Perfekt"
-      value = "PERFEKT">
-      </v-checkbox>
-       <v-checkbox
-    v-model="temposVerbais"
-      label="Plusquamperferkt"
-      value = "PLUSQUAMPERFEKT">
-      </v-checkbox>
-    </v-col>
+  <v-card color="#FFE8D6">
+    <p id="instrucao">Selecione os tempos verbais que você quer praticar:</p>
+    <v-container>
+      <v-row justify="center">
+        <v-col md="4">
+          <h2 class="tituloTempos">Indicativ</h2>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Prasens"
+            value="PRASENS"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Prateritum"
+            value="PRATERITUM"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Futur1"
+            value="FUTUR1"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Futur2"
+            value="FUTUR2"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Perfekt"
+            value="PERFEKT"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Plusquamperferkt"
+            value="PLUSQUAMPERFEKT"
+            color="#6B705C"
+          >
+          </v-checkbox>
+        </v-col>
 
-    <v-col>
-   <h2 class = "tituloTempos"> Konjunktiv1 </h2>
-    <v-checkbox
-    v-model="temposVerbais"
-      label="Prasens"
-      value = "KONJUNKTIV1_PRASENS">
-      </v-checkbox>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Futur1"
-      value = "KONJUNKTIV1_FUTUR1">
-      </v-checkbox>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Perfekt"
-      value = "KONJUNKTIV1_PERFEKT">
-      </v-checkbox>
-    </v-col>
-    
-    <v-col>
-           <h2 class = "tituloTempos"> Konjunktiv2 </h2>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Prateritum"
-      value = "KONJUNKTIV2_PRATERITUM">
-      </v-checkbox>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Futur1"
-      value = "KONJUNKTIV2_FUTUR1">
-      </v-checkbox>
-      <v-checkbox
-    v-model="temposVerbais"
-      label="Futur2"
-      value = "KONJUNKTIV2_FUTUR2">
-      </v-checkbox>
-    </v-col>
-        </v-row>
+        <v-col md="4">
+          <h2 class="tituloTempos">Konjunktiv1</h2>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Prasens"
+            value="KONJUNKTIV1_PRASENS"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Futur1"
+            value="KONJUNKTIV1_FUTUR1"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Perfekt"
+            value="KONJUNKTIV1_PERFEKT"
+            color="#6B705C"
+          >
+          </v-checkbox>
+        </v-col>
 
-    </v-card>
-
+        <v-col md="4">
+          <h2 class="tituloTempos">Konjunktiv2</h2>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Prateritum"
+            value="KONJUNKTIV2_PRATERITUM"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Futur1"
+            value="KONJUNKTIV2_FUTUR1"
+            color="#6B705C"
+          >
+          </v-checkbox>
+          <v-checkbox
+            v-model="temposVerbais"
+            label="Futur2"
+            value="KONJUNKTIV2_FUTUR2"
+            color="#6B705C"
+          >
+          </v-checkbox>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
 export default {
-    data:function(){
-        return {
-            temposVerbais:[]
-        }
-    }
-    
-}
+  data: function () {
+    return {
+      temposVerbais: [],
+    };
+  },
+};
 </script>
 
 <style>
+h2.tituloTempos {
+  color: #4c4f40;
+}
 
+p#instrucao {
+  padding: 5px;
+  font-size: 20px;
+  text-align: center;
+  color: #4c4f40;
+  font-weight: bold;
+}
 </style>
